66e98793bcd82877b377b37b3d630f5b56d8e50e
Marwane Ezzaze <git@ezzaze.com>
2023-01-11 18:50:07 +0100

    Updates on unit tests

--- tests/binarySearchTest.php
+++ tests/binarySearchTest.php
@@ -3,21 +3,21 @@
 use Ezzaze\BinarySearch\BinarySearch;
 
 it('can find item in array', function () {
-    $haystack = range(0, 999999);
+    $haystack = range(0, 99999);
     $result = BinarySearch::exists(6000, $haystack);
 
     expect($result)->toBeTrue();
 });
 
 it('cannot find item in array', function () {
-    $haystack = range(0, 999999);
-    $result = BinarySearch::exists(1000000, $haystack);
+    $haystack = range(0, 99999);
+    $result = BinarySearch::exists(100000, $haystack);
 
     expect($result)->toBeFalse();
 });
 
 it('cannot find item in a disarray', function () {
-    $haystack = range(0, 999999);
+    $haystack = range(0, 99999);
     shuffle($haystack);
     $result = BinarySearch::exists(1, $haystack, false);
 
@@ -25,14 +25,14 @@ it('cannot find item in a disarray', function () {
 });
 
 it('can find subarray in array', function () {
-    $haystack = array_chunk(range(0, 999999), 2);
+    $haystack = array_chunk(range(0, 99999), 2);
     $result = BinarySearch::exists([60000, 60001], $haystack);
 
     expect($result)->toBeTrue();
 });
 
 it('cannot find subarray in disarray', function () {
-    $haystack = array_chunk(range(0, 999999), 2);
+    $haystack = array_chunk(range(0, 99999), 2);
     shuffle($haystack);
     $result = BinarySearch::exists([60000, 60001], $haystack, false);
     expect($result)->toBeFalse();
